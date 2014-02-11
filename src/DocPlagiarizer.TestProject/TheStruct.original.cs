732263045e8106a72f8e3ce1de49160f44c405f0
Peter MacNaughton <peter@macnaughton.info>
2014-02-11 12:40:14 -0700

    Added a struct to the TestProject

---
+++ src/DocPlagiarizer.TestProject/TheStruct.original.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocPlagiarizer.TestProject
+{
+    public class TheClass : TheInterface
+    {
+        public void Method(string argument) { throw new NotImplementedException(); }
+
+        public string Property { get; set; }
+
+        public event EventHandler TheEvent;
+    }
+}
